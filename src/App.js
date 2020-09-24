762374583938b490345260c1e4d8ae4c156c142a
Raushan <raushankumar606@gmail.com>
2020-09-24 14:29:19 +0530

    tools setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
-const Pet = ({ name, animal, breed }) => {
-  return React.createElement("div", {}, [
-    React.createElement("h2", {}, name),
-    React.createElement("h3", {}, animal),
-    React.createElement("h3", {}, breed),
-  ]);
-};
+import React from "react";
+import { render } from "react-dom";
+import Pet  from "./Pet";
 
 const App = () => {
   return React.createElement("div", {}, [
@@ -15,4 +11,4 @@ const App = () => {
   ]);
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(React.createElement(App), document.getElementById("root"));
